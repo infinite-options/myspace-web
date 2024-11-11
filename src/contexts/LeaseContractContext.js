0120e3edf7c9c1b4db82f7a6be047ac3a79d7b09
pmarathay <pmarathay@infiniteoptions.com>
2024-11-11 09:49:39 -0800

    working on Lease Widget

---
+++ src/contexts/LeaseContractContext.js
@@ -0,0 +1,113 @@
+import React, { createContext, useState, useEffect } from "react";
+import APIConfig from "../utils/APIConfig";
+import { useUser } from "./UserContext";
+const LeaseContractContext = createContext();
+
+export const LeaseContractProvider = ({ children }) => {
+  const { getProfileId } = useUser();
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [defaultContractFees, setDefaultContractFees] = useState([]);
+  const [allContracts, setAllContracts] = useState([]);
+  const [currentContractUID, setCurrentContractUID] = useState("");
+  const [currentContractPropertyUID, setCurrentContractPropertyUID] = useState("");
+  const [contractRequests, setContractRequests] = useState([]);
+  const [isChange, setIsChange] = useState(false);
+
+  const fetchDefaultContractFees = async () => {
+    try {
+      const response = await fetch(`${APIConfig.baseURL.dev}/profile/${getProfileId()}`);
+      const data = await response.json();
+      // console.log("DATA PROFILE", data);
+
+      if (data?.profile?.result && data?.profile?.result?.length > 0) {
+        const profileFees = data?.profile?.result[0].business_services_fees ? JSON.parse(data?.profile?.result[0].business_services_fees) : [];
+
+        setDefaultContractFees(profileFees);
+      }
+    } catch (error) {
+      console.error("Error fetching profile data: ", error);
+    }
+  };
+
+  const fetchContracts = async () => {
+    const result = await fetch(`${APIConfig.baseURL.dev}/contracts/${getProfileId()}`);
+    const data = await result.json();
+
+    if (data !== "No records for this Uid") {
+      setAllContracts(data.result);
+      const newAndSentContracts = data?.result?.filter((contract) => contract.contract_status === "NEW" || contract.contract_status === "SENT");
+      setContractRequests(newAndSentContracts);
+
+      // Set currentContractUID and currentContractPropertyUID after the fetch
+      // if (!currentContractUID && !currentContractPropertyUID && data.result.length > 0) {
+      //   setCurrentContractUID(data.result[0].contract_uid);
+      //   setCurrentContractPropertyUID(data.result[0].contract_property_id);
+      // }
+    }
+  };
+
+  // const fetchContractRequests = async () => {
+  //   const response = await fetch(`${APIConfig.baseURL.dev}/dashboard/${getProfileId()}`);
+  //   // const response = await fetch(`${APIConfig.baseURL.dev}/dashboard/600-000003`);
+
+  //   try {
+  //     const jsonData = await response.json();
+  //     const requests = jsonData?.newPMRequests?.result;
+  //     setContractRequests(requests);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    const loadData = async () => {
+      if (!dataLoaded) {
+        // setDataLoaded(true);
+
+        await fetchDefaultContractFees();
+        await fetchContracts();
+        // fetchContractRequests();
+        setDataLoaded(true);
+      }
+    };
+    loadData();
+  }, [dataLoaded]);
+
+  useEffect(() => {
+    // console.log("LeaseContractContext - currentContractUID - ", currentContractUID);
+  }, [currentContractUID]);
+
+  useEffect(() => {
+    // console.log("LeaseContractContext - currentContractPropertyUID - ", currentContractPropertyUID);
+  }, [currentContractPropertyUID]);
+
+  const updateContractUID = (uid) => {
+    setCurrentContractUID(uid);
+  };
+
+  const updateContractPropertyUID = (uid) => {
+    setCurrentContractPropertyUID(uid);
+  };
+
+  return (
+    <LeaseContractContext.Provider
+      value={{
+        defaultContractFees,
+        allContracts,
+        contractRequests,
+        currentContractUID,
+        updateContractUID,
+        currentContractPropertyUID,
+        updateContractPropertyUID,
+        isChange,
+        setIsChange,
+        dataLoaded,
+        fetchContracts,
+      }}
+    >
+      {children}
+    </LeaseContractContext.Provider>
+  );
+};
+
+export default LeaseContractContext;
