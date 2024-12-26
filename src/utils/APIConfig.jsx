dcfb12bcf57a9484c14f53e203a27bd58a8b353c
dhyey510 <dhyey870@gmail.com>
2024-12-26 12:57:04 -0800

    change production endpoint in apiconfig file

--- src/utils/APIConfig.jsx
+++ src/utils/APIConfig.jsx
@@ -5,7 +5,7 @@ const APIConfig = {
   //   dev: "https://mfs1128sspm.manifestmy.space",
   // },
   baseURL: {
-    dev: isDebug? "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev" : "https://mfs1128sspm.manifestmy.space",
+    dev: isDebug? "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev" : "https://qn4agnb0v9.execute-api.us-west-1.amazonaws.com/production",
   }
   // baseURL: {
   //   dev: "http://127.0.0.1:4010",
