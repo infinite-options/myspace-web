a775a339030aed50483f3b79f8518f6f9fb5fb00
dhyey510 <dhyey870@gmail.com>
2024-12-26 11:30:11 -0800

    change apiconfig.js file for debug endpoint logic

--- src/utils/APIConfig.jsx
+++ src/utils/APIConfig.jsx
@@ -1,9 +1,11 @@
+const isDebug = process.env.REACT_APP_DEBUG === 'true';
+
 const APIConfig = {
   // baseURL: {
   //   dev: "https://mfs1128sspm.manifestmy.space",
   // },
   baseURL: {
-    dev: "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev",
+    dev: isDebug? "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev" : "https://mfs1128sspm.manifestmy.space",
   }
   // baseURL: {
   //   dev: "http://127.0.0.1:4010",
