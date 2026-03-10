5957c6a3e41eabd4f3b3349273a23cb80819be5e
pmarathay <pmarathay@infiniteoptions.com>
2026-03-09 19:29:59 -0700

    updating app.config for live site

--- src/utils/APIConfig.jsx
+++ src/utils/APIConfig.jsx
@@ -9,13 +9,13 @@ const APIConfig = {
   //   dev: "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev",
   // },
 
-  // baseURL: {
-  //   dev: encryptionON ? "https://qn4agnb0v9.execute-api.us-west-1.amazonaws.com/production" : "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev",
-  // },
-
   baseURL: {
-    dev: "http://127.0.0.1:4010",
+    dev: encryptionON ? "https://qn4agnb0v9.execute-api.us-west-1.amazonaws.com/production" : "https://l0h6a9zi1e.execute-api.us-west-1.amazonaws.com/dev",
   },
+
+  // baseURL: {
+  //   dev: "http://127.0.0.1:4010",
+  // },
 };
 
 export default APIConfig;
